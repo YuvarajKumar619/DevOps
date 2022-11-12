260dc28906fe52c766eeb7a3c6968028603e770e
Yuvaraj <yuviyuvi619@gmail.com>
2022-11-12 15:09:34 +0530

    added if condition to check the data

--- src/main/java/com/example/DevOps/Controller/RestAPI.java
+++ src/main/java/com/example/DevOps/Controller/RestAPI.java
@@ -1,16 +1,22 @@
 package com.example.DevOps.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping ("Common")
+@RequestMapping("Common")
 public class RestAPI {
 
-	@GetMapping ("WelcomePage")
+	@GetMapping("WelcomePage")
 	public String welcomePage() {
 		return " Welcom To This Page ";
+	}
+
+	@GetMapping("PathVariable/{value}")
+	public String usingPathVariable(@PathVariable String value) {
+		return " This is the value --> "+value;
 	}
 
 }
